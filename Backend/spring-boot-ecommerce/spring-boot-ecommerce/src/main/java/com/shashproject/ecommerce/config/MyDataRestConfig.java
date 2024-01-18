af4193c4218345471aa278cfe5095517fe38e28c
Shashwat_CodeReaper <shashwatsingh13@gmail.com>
2024-01-18 18:41:54 +0530

    Modify myDaraRestConfig.java file to expose category ids, in Frontend created Product-category-menu component and product category class, changes in app.component.html, product.service.ts, product-categpry.ts

--- Backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/shashproject/ecommerce/config/MyDataRestConfig.java
+++ Backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/shashproject/ecommerce/config/MyDataRestConfig.java
@@ -1,6 +1,12 @@
 package com.shashproject.ecommerce.config;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -10,8 +16,19 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import com.shashproject.ecommerce.entity.Product;
 import com.shashproject.ecommerce.entity.ProductCategory;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager){
+         entityManager = theEntityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors ){
     
@@ -27,6 +44,30 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         .forDomainType(ProductCategory.class)
         .withItemExposure((metdata,HttpMethods)-> HttpMethods.disable(theunsupportedActions))
         .withCollectionExposure((metdata, HttpMethods)-> HttpMethods.disable(theunsupportedActions));
+
+
+        // call an internal helper method
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        
+        // expose entity ids
+
+        // - get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // - create an array of the enitiy types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // - get the eneitity types for the entities
+        for(EntityType temEntityType : entities){
+            entityClasses.add(temEntityType.getJavaType());
+        }
+
+        // - expose the enitity ids for the array of enitity/domain type
+        Class[] domainTypes =entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
     
 }
--- Frontend/angular-ecommerce/src/app/services/product.service.ts
+++ Frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -3,13 +3,18 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import {map} from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  
 
   private baseUrl ="http://localhost:8080/api/products";
+
+  private categoryUrl = "http://localhost:8080/api/product-category";
+
   constructor(private httpClient: HttpClient) { }
 
   // return an Observable { map the json data from spring Data REST to product array}
@@ -17,15 +22,31 @@ export class ProductService {
        // need to build URL based on category id 
        const searchUrl =`${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response =>response._embedded.products)
+    );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    
+    // call rest API and returns an observable 
+    // Maps the JSON data from Spring Data REST to ProductCategory array
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+     map(response => response._embedded.productCategory)
     );
   }
 }
 
 // unwrap the json from spring data REST _embedded entry
-interface GetResponse{
+interface GetResponseProducts{
   _embedded:{
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory{
+  // unwrap the json from spring data REST _embedded entry
+  _embedded:{
+    productCategory: ProductCategory[];
   }
 }
